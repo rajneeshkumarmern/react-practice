190751c72beaa2fde42c8fa2ac5ee5f876322d43
rajneeshkumar1999-tech <rajneesh.tech.designer@gmail.com>
2026-03-11 00:04:33 +0530

    11-components example

--- 01-jsx/src/App.jsx
+++ 01-jsx/src/App.jsx
@@ -1,46 +1,16 @@
-import CourseList from "./Course/CourseList";
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import DashBoard from "./DashBoard/DashBoard";
 
 function App() {
-
-  const mycourseList = [
-    { cid: 101, cname: "Angular", trainer: "Srinivas", price: 15000, isOnline: true },
-    { cid: 102, cname: "React JS", trainer: "Srinivas", price: 15000, isOnline: false },
-    { cid: 103, cname: "Java FSD", trainer: "Srinivas", price: 55000, isOnline: false },
-    { cid: 104, cname: "Spring Boot", trainer: "Srinivas", price: 25000, isOnline: true },
-    { cid: 105, cname: "MicroServices", trainer: "Srinivas", price: 225000, isOnline: true }
-  ];
-
   return (
-    <div>
+    <div className="container-fluid">
 
-      <p className="text-center">Welcome to Java Learning Center</p>
+      <h1 className="text-center">
+        Welcome to CourseCube !!!
+      </h1>
 
-      <ul className="nav justify-content-end">
-
-        <li className="nav-item">
-          <a className="nav-link active">Home</a>
-        </li>
-
-        <li className="nav-item">
-          <a className="nav-link active">About</a>
-        </li>
-
-        <li className="nav-item">
-          <a className="nav-link active">Courses</a>
-        </li>
-
-        <li className="nav-item">
-          <a className="nav-link active">Contact</a>
-        </li>
-
-      </ul>
-
-      <br />
-
-      <div className="container-fluid">
-        <CourseList mycourses={mycourseList} />
-      </div>
+      <DashBoard />
 
     </div>
   );
